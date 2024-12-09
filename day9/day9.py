ca6c5dc867be2aec78b55d74124e3a8f5f4fc95a
elio27 <67876496+elio27@users.noreply.github.com>
2024-12-09 19:07:36 +0100

    Create day9.py
    Blud lost his mind on this one

---
+++ day9/day9.py
@@ -0,0 +1,73 @@
+def load_map(filepath):
+  with open(filepath, 'r') as f:
+    data = f.read().replace("\n","")
+  id = 0
+  map = []
+  for i in range(len(data)):
+    if i%2:
+      map.append(["."]*int(data[i]))
+    else:
+      map.append([str(id)]*int(data[i]))
+      id +=1
+  return map
+
+
+def unpack(grid):
+  newgrid = []
+  for bl in grid:
+    for e in bl:
+      newgrid.append(e)
+  return newgrid
+
+
+def fill_block(free, data):
+  u0 = free.index(".")
+  newblock = free
+  for i in range(len(data)):
+    newblock[u0 + i] = data[i]
+  return newblock
+
+
+def free_space(block):
+  return block.count(".")
+
+
+
+
+
+## Part I
+grid = unpack(load_map("/input 3.txt"))
+while grid[-1]==".": grid.pop()
+free = [i for i, c in enumerate(grid) if c=="."]
+for f in free:
+  while grid[-1]==".": grid.pop()
+  if "." in grid:
+    grid[f] = grid.pop()
+  else:
+    break
+
+s = 0
+for i in range(len(grid)):
+  s += int(grid[i])*i
+print(s)
+
+
+
+## Part II
+grid = load_map("/input 3.txt")
+for ni in range(0, len(grid), 2)[::-1]:
+  print(ni/len(grid))
+  n = grid[ni]
+  for bi in range(1, ni, 2):
+    b = grid[bi]
+    if free_space(b) >= len(n):
+      #print(b, n)
+      grid[bi] = fill_block(b, n)
+      grid[ni] = ["."]*len(n)
+      break
+
+s = 0
+for i in range(len(grid)):
+  s += int(grid[i])*i
+print(s)
+
